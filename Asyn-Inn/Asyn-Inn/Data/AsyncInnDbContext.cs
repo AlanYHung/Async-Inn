62ee0a871f811f9f3881cbea7e092c4ef1a8a3ab
AlanYHung <72707899+AlanYHung@users.noreply.github.com>
2021-01-26 15:49:21 -0800

    Database and Models Added

---
+++ Asyn-Inn/Asyn-Inn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Asyn_Inn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asyn_Inn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public AsyncInnDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
